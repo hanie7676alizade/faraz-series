f3c04ebbba3a4ba416a075a74a5386f4dbf39fce
Hanie <hanieh7676alizadeh@gmail.com>
2024-06-28 15:49:19 +0330

    feat(homePage): handle creating axios instance and adding serial services

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,5 @@
+import { SerialType } from "../types/types";
+
 export function toDate(time: number | undefined | null) {
   if (!time) return new Date().toLocaleDateString("fa");
   return new Date(time).toLocaleDateString("fa");
@@ -24,3 +26,21 @@ function getDateFormat(
   const date = new Intl.DateTimeFormat("fa-IR", option).format(uDate);
   return date;
 }
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const mapToSerialType = (data: any[]): SerialType[] => {
+  return data.map((_item) => {
+    const item = _item.show ?? _item;
+    console.log({ item });
+    return {
+      id: item.id,
+      name: item.name,
+      genres: item.genres,
+      image: item.image ? item.image.medium : "",
+      summary: item.summary,
+      imdb: item.rating ? item.rating.average : 0,
+      ended: item.ended,
+    };
+  });
+  // return {};
+};
